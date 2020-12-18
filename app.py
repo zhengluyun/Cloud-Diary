184fd0c65e128dbe779c0e260d5f4718b3921e6a
zhengluyun <zlystevens@gmail.com>
2020-12-18 18:21:20 -0500

    Add gunicorn + requirments of pip

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/zhengluyun/Downloads/Cloud Diary/diary.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/zhengluyun/Desktop/Cloud-Diary/diary.db'
 
 db = SQLAlchemy(app)
 
